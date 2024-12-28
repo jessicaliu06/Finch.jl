0c8ab41a9341d5901a7aa5bab32f04e1c1bc8217
Willow Ahrens <willow@csail.mit.edu>
2024-12-28 16:36:17 -0500

    add evaluation count to benchmark setup

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -98,7 +98,8 @@ for (scheduler_name, scheduler) in [
                 begin
                     @einsum y[i] += A[i, j] * x[j]
                 end,
-                setup = (A = Tensor(Dense(SparseList(Element(0.0))), fsprand(1, 1, 1)); x = rand(1))
+                setup = (A = Tensor(Dense(SparseList(Element(0.0))), fsprand(1, 1, 1)); x = rand(1)),
+                evals = 1
             )
         end
 

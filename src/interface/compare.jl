151a665c799541582aab7a7930fa784e04e35991
GenericP3rson <genericp3rson@gmail.com>
2025-03-13 16:44:47 -0400

    [wip] argmin updates

--- src/interface/compare.jl
+++ src/interface/compare.jl
@@ -56,28 +56,31 @@ function Base.isequal(A::AbstractArray, B::AbstractTensor)
     return helper_isequal(A, B)
 end
 
-@staged function helper_argmin(A)
+@staged function helper_argmin(A, dim)
    idxs = [Symbol(:i_, n) for n in 1:ndims(A)]
    exts = Expr(:block, (:($idx = _) for idx in reverse(idxs))...)
    return quote
-       x = Scalar(Inf => CartesianIndex(zeros(Int, ndims(A))...))
+       x = [Scalar(Inf => CartesianIndex(zeros(Int, ndims(A))...)) for _ in 1:ndims(A)] # for _ in 1:ndims(A)
        @finch $(Expr(
            :for,
            exts,
            quote
-               x[] <<minby>>= A[$(idxs...)] => CartesianIndex($(idxs...))
+               x[1][] <<minby>>= A[$(idxs...)] => CartesianIndex($(idxs...))
+               # if x[] < A[$(idxs...)] 
+               #      x[] = A[$(idxs...)] => CartesianIndex($(idxs...))
+               # end
            end,
        ))
        return x
    end
 end
 
-function Base.argmin(A::AbstractTensor)
-    return helper_argmin(A)
+function Base.argmin(A::AbstractTensor, dim::int)
+    return helper_argmin(A, dim)
 end
 
-function Base.argmin(A::AbstractArray)
-    return helper_argmin(A)
+function Base.argmin(A::AbstractArray, dim::int)
+    return helper_argmin(A, dim)
 end
 
 @staged function helper_argmax(A)
@@ -96,10 +99,10 @@ end
    end
 end
 
-function Base.argmax(A::AbstractTensor)
-    return helper_argmax(A)
+function Base.argmax(A::AbstractTensor, dim::int)
+    return helper_argmax(A, dim)
 end
 
-function Base.argmax(A::AbstractArray)
-    return helper_argmax(A)
+function Base.argmax(A::AbstractArray, dim::int)
+    return helper_argmax(A, dim)
 end

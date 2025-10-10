c8583197fd0cdcafbfa4f9f5b04d32f158295e6c
Jessica Liu <jessicatracyliu@gmail.com>
2025-10-10 01:39:59 -0400

    Generalize SuperTensor einsum function

--- src/tensors/supertensor.jl
+++ src/tensors/supertensor.jl
@@ -1,8 +1,10 @@
+using Debugger
+
 """
     SuperTensor{N,B,T} <: AbstractArray{T,N}
 
 `SuperTensor{N,B,T}` is an abstract representation of a high-order tensor
-with `N` dimensions, where `B` is number of modes of the underlying base array 
+with order `N`, where `B` is number of modes of the underlying base array 
 and `T` is the type of the elements in the array.
 """
 struct SuperTensor{N,B,T} <: AbstractArray{T,N}
@@ -45,7 +47,12 @@ function SuperTensor(arr::AbstractArray{T,N}, map::NTuple{N,Int}) where {T,N}
 
     # Compute the shape of the base tensor by multiplying together the sizes of the logical modes that map to each base mode.
     B = maximum(map)
-    base_shape = ntuple(b -> prod(shape[d] for d in 1:N if map[d] == b), B)
+    base_shape = ntuple(
+        b -> begin
+            dims = [shape[d] for d in 1:N if map[d] == b]
+            isempty(dims) ? 1 : prod(dims)
+        end, B
+    )
 
     # Permute the array so that logical modes mapped to the same base mode are next to each other.
     groups = [findall(d -> map[d] == b, 1:N) for b in 1:B]      # Group logical modes by their corresponding base mode.
@@ -55,6 +62,74 @@ function SuperTensor(arr::AbstractArray{T,N}, map::NTuple{N,Int}) where {T,N}
     # Reshape the permuted array into the shape of the base tensor.
     base = reshape(permuted_arr, base_shape)
     return SuperTensor{N,B,T}(shape, base, map)
+end
+
+function SuperTensor(arr::AbstractArray{T,N}, map::NTuple{N,Symbol}, base_idcs::Vector{Symbol}) where {T,N}
+    # Get the dimensions of the input tensor.
+    shape = size(arr)
+
+    # Compute the shape of the base tensor by multiplying together the sizes of the logical modes that map to each base index.
+    base_shape = ntuple(i -> begin
+        b = base_idcs[i]
+        dims = [shape[d] for d in 1:N if map[d] == b]
+        isempty(dims) ? 1 : prod(dims)
+    end, length(base_idcs))
+
+    # Permute the array so that logical modes mapped to the same base index are next to each other.
+    groups = [findall(d -> map[d] == b, 1:N) for b in base_idcs]    # Group logical modes by their corresponding base index.
+    perm = vcat(groups...)                                          # Create a permutation of the modes that places logical modes that are going to the same base index next to each other.
+    permuted_arr = permutedims(arr, perm)                           # Permute (i.e., transpose) the array according to this permutation.
+
+    # Reshape the permuted array into the shape of the base tensor.
+    base = reshape(permuted_arr, base_shape)
+
+    # Construct a map in the right format for the underlying struct.
+    symbol_to_number = Dict{Symbol, Int}()
+    for (i, sym) in enumerate(base_idcs)
+        symbol_to_number[sym] = i
+    end
+    final_map = ntuple(n -> symbol_to_number[map[n]], N)
+
+    B = length(base_idcs)
+    return SuperTensor{N,B,T}(shape, base, final_map)
+end
+
+function SuperTensor(arr::AbstractArray{T,N}, base_idcs::Vector{Tuple{Symbol, Vector{Int}}}) where {T,N}
+    # Get the dimensions of the input tensor.
+    shape = size(arr)
+
+    # Map each logical mode to its corresponding base index symbol.
+    logical_modes_map = Dict{Int, Symbol}()   
+    for (sym, logical_modes) in base_idcs
+        for m in logical_modes
+            logical_modes_map[m] = sym
+        end
+    end
+
+    # Compute the shape of the base tensor by multiplying together the sizes of the logical modes that correspond to each base index.
+    base_shape = ntuple(i -> begin
+        base_idx, logical_modes = base_idcs[i]
+        dims = [shape[mode] for mode in logical_modes]
+        isempty(dims) ? 1 : prod(dims)
+    end, length(base_idcs))
+
+    # Permute the array so that logical modes mapped to the same base index are next to each other.
+    groups = [logical_modes for (_, logical_modes) in base_idcs]   # Group logical modes by their corresponding base index in the given orders of base indices and logical modes.
+    perm = vcat(groups...)                                         # Create a permutation of the modes in the order specified.
+    permuted_arr = permutedims(arr, perm)                          # Permute (i.e., transpose) the array according to this permutation.
+
+    # Reshape the permuted array into the shape of the base tensor.
+    base = reshape(permuted_arr, base_shape)
+
+    # Construct a map in the right format for the underlying struct.
+    symbol_to_number = Dict{Symbol, Int}()
+    for (i, (sym, _)) in enumerate(base_idcs)
+        symbol_to_number[sym] = i
+    end
+    final_map = ntuple(n -> symbol_to_number[logical_modes_map[n]], N)
+
+    B = length(base_idcs)
+    return SuperTensor{N,B,T}(shape, base, final_map)
 end
 
 """
@@ -96,7 +171,7 @@ function Base.getindex(A::SuperTensor{N,B}, I::Vararg{Int, N}) where {N,B}
     return A.base[base_idcs...]
 end
 
-function einsum(C_idcs::Vector{Symbol}, A::Array, A_idcs::Vector{Symbol}, B::Array, B_idcs::Vector{Symbol}, extra_C_dims::Dict{Symbol,Int}=Dict{Symbol,Int}())
+function einsum(C_idcs::Vector{Symbol}, A::Array, A_idcs::Vector{Symbol}, B::Array, B_idcs::Vector{Symbol})
     all_idcs = union(A_idcs, B_idcs, C_idcs)            # All indices that appear in A, B, or C.
     sum_idcs = setdiff(all_idcs, C_idcs)                # We will sum over indices which are NOT in C.
 
@@ -106,15 +181,6 @@ function einsum(C_idcs::Vector{Symbol}, A::Array, A_idcs::Vector{Symbol}, B::Arr
     end
     for (sym, size) in zip(B_idcs, size(B))
         ranges[sym] = 1:size
-    end
-
-    if extra_C_dims !== nothing && !isempty(extra_C_dims)
-        # Also add any extra indices in C which are not present in A or B.
-        for sym in C_idcs
-            if !haskey(ranges, sym) && haskey(extra_C_dims, sym)
-                ranges[sym] = 1:extra_C_dims[sym]
-            end
-        end
     end
 
     # Determine the shape of C by finding the sizes of the indices in C.
@@ -145,59 +211,162 @@ function einsum(C_idcs::Vector{Symbol}, A::Array, A_idcs::Vector{Symbol}, B::Arr
     return C
 end
 
-function supertensor_einsum(C_idcs::Vector{Symbol}, A::Array, A_idcs::Vector{Symbol}, B::Array, B_idcs::Vector{Symbol}, extra_C_dims::Dict{Symbol,Int}=Dict{Symbol,Int}())
-    # Objective: Reshape operands A and B into SuperTensors order-2 base tensors, then perform einsum on the base tensors.
-    # Final format: C_ijk = A_ip B_pj      
+"""
+    supertensor_einsum(C_idcs::Vector{Symbol}, inputs::Vector{Tuple{Array, Vector{Symbol}}})
 
-	A_and_B_idcs = sort(collect(intersect(A_idcs, B_idcs)))     # Indices that appear in both A and B.  (will be grouped into index p)
-    A_free_idcs = sort(collect(setdiff(A_idcs, B_idcs)))        # Indices that appear only in A and C.  (will be grouped into index i)
-    B_free_idcs = sort(collect(setdiff(B_idcs, A_idcs) ))       # Indices that appear only in B and C.  (will be grouped into index j)
-    C_only_idcs = setdiff(C_idcs, union(A_idcs, B_idcs))        # Indices that appear only in C.        (will be grouped into index k)
+NOTE: This function assumes that there are two input tensors, but the logic is generalized for more than two inputs.
+"""
+function supertensor_einsum(output_idcs::Vector{Symbol}, inputs::Vararg{Tuple{Array, Vector{Symbol}}})
+    # ========== STEP 1 ==========
+    # Determine the set of tensors that each index appears in.
 
-    perm_A = vcat([findfirst(==(sym), A_idcs) for sym in A_free_idcs],
-                  [findfirst(==(sym), A_idcs) for sym in A_and_B_idcs])
-
-    perm_B = vcat([findfirst(==(sym), B_idcs) for sym in A_and_B_idcs],
-                  [findfirst(==(sym), B_idcs) for sym in B_free_idcs])
-
-    perm_C = vcat([findfirst(==(sym), C_idcs) for sym in A_free_idcs],
-                  [findfirst(==(sym), C_idcs) for sym in B_free_idcs],
-                  [findfirst(==(sym), C_idcs) for sym in C_only_idcs])
-
-    A_perm = permutedims(A, perm_A)
-    B_perm = permutedims(B, perm_B)
-
-    A_idcs_perm = A_idcs[perm_A]
-    B_idcs_perm = B_idcs[perm_B]
-    C_idcs_perm = C_idcs[perm_C]
-
-    mapA = ntuple(length(A_idcs)) do i
-        if A_idcs_perm[i] in A_free_idcs
-            1
-        else
-            2
+    idx_appearances = Dict{Symbol, Set{Symbol}}()   # Maps each index symbol to the set of tensors it appears in
+                                                    # e.g. :i -> Set([:T1, :T2]), :j -> Set([:T1, :out]), etc.
+    
+    # Iterate over the index lists of all input tensors in order to collect the tensors in which each index appears.
+    for (i, (_, idcs)) in enumerate(inputs)        
+        tensor_name = Symbol("T", i)                                        # Name the input tensors in the order provided: :T1, :T2, etc.
+        for s in idcs
+            tensor_appearances = get!(idx_appearances, s, Set{Symbol}())    # Get the set of tensors in which this index appears.  
+            push!(tensor_appearances, tensor_name)                          # Add the current tensor to this set of appearances.
         end
     end
+    @bp
+    # Similarly, record the indices which appear in the output.
+    for s in output_idcs
+        tensor_appearances = get!(idx_appearances, s, Set{Symbol}())
+        push!(tensor_appearances, :out)                                     # Name the output tensor :out.
+    end
+    @bp
 
-    mapB = ntuple(length(B_idcs)) do i
-        if B_idcs_perm[i] in A_and_B_idcs
-            1
-        else
-            2
+    # ========== STEP 2 ==========
+    # Group the indices which appear in exactly the same set of tensors. These groups will later be merged into single indices in the SuperTensors.
+
+    idx_groups = Dict{Set{Symbol}, Vector{Symbol}}()        # Maps each unique set of tensor appearances to the group of index symbols that appear in exactly that set of tensors.
+                                                            # e.g. Set([:T1, :T2]) -> [:i, :j], Set([:T1, :out]) -> [:k], etc.
+    
+    # Assign each index to a group based on the set of tensors it appears in.
+    for (idx, appearances) in idx_appearances
+        idx_group = get!(idx_groups, appearances, Symbol[])
+        push!(idx_group, idx)
+    end
+    @bp
+
+    # ========== STEP 3 ==========
+    # Assign each group a new index symbol. We identify each group by the set of tensors that the indices in that group appear in.
+
+    # Collect tensor sets (corresonding to index groups) and assign each tensor set (and thereby its index group) a new index symbol.
+    tensor_sets = collect(keys(idx_groups));          
+    new_idx_map = Dict{Set{Symbol}, Symbol}()               # Map each unique set of tensors (and thereby its index group) to a new index symbol (for the merged index group).
+                                                            # e.g. Set([:T1, :T2]) -> :i1, Set([:T1, :out]) -> :i2, etc.
+    for (k, tensor_set) in enumerate(tensor_sets)
+        new_idx_map[tensor_set] = Symbol("i", k)
+    end
+    @bp
+
+    # ========== STEP 4 ==========
+    # Construct SuperTensors for each input tensor, along with the merged indices for its base tensor.
+
+    # IMPORTANT: When reshaping an input tensor into its base tensor, we must ensure that the mapping from
+    # logical modes to base modes is consistent in all tensors. Specifically, when we flatten a group of indices,
+    # we must ensure that it is flattened in the same order in all tensors. 
+
+    # IMPORTANT: When reshaping an input tensor into its base tensor, we need to ensure that index groups that should
+    # summed over are adjacent, so that the dimensions are compatible.
+
+    superTensors = Vector{SuperTensor}()
+    input_idcs = Vector{Vector{Symbol}}()
+
+    for (i, (arr, idcs)) in enumerate(inputs)
+        tensor_name = Symbol("T", i)                                # Name the input tensors in the order provided: :T1, :T2, etc.
+                                                                    # This is consistent with how the input tensors were named when determining index appearances in Step 1.
+ 
+        # Map each logical mode in this tensor to its base index symbol.
+        logical_mode_map = ntuple(d ->
+            begin
+                idx = idcs[d]                       # Get the index symbol for this logical mode.
+                appearances = idx_appearances[idx]  # Find the set of tensors that this index appears in.
+                return new_idx_map[appearances]     # Find the new index symbol assigned to this set of tensors.
+            end, length(idcs))
+        @bp
+
+        # Map base index symbols to the list of logical modes that map to this base index.
+        base_symbols_to_logical_modes = Dict{Symbol, Vector{Int}}()
+        for (logical_mode, sym) in enumerate(logical_mode_map)
+            logical_modes = get!(base_symbols_to_logical_modes, sym, Int[])
+            push!(logical_modes, logical_mode)
         end
+        @bp
+
+        # Convert the map into a vector of tuples in the form needed for the constructor.
+        base_idcs = [(sym, base_symbols_to_logical_modes[sym]) for sym in collect(keys(base_symbols_to_logical_modes))]
+
+        # Construct the SuperTensor
+        push!(superTensors, SuperTensor(arr, base_idcs))
+        push!(input_idcs, [sym for (sym, _) in base_idcs])
+        @bp
     end
 
-    superTensorA = SuperTensor(A_perm, mapA)
-    superTensorB = SuperTensor(B_perm, mapB)
+    # ========== STEP 5 ==========
+    # Construct the map for the output SuperTensor, along with the merged indices for its base tensor.
 
-    A_base_idcs = [:i, :p]
-    B_base_idcs = [:p, :j]
-    C_base_idcs = !isempty(extra_C_dims) ? [:i, :j, :k] : [:i, :j]
+    # Map each logical mode in the output tensor to its base index symbol.
+    output_logical_mode_map = ntuple(i -> 
+        begin
+            idx = output_idcs[i]                  # Get the index symbol for this logical mode.
+            appearances = idx_appearances[idx]    # Find the set of tensors that this index appears in.
+            new_idx_map[appearances]              # Find the new index symbol assigned to this set of tensors.
+        end, length(output_idcs))
 
-    C_base = einsum(C_base_idcs,
-                    superTensorA.base, A_base_idcs,
-                    superTensorB.base, B_base_idcs,
-                    extra_C_dims!==nothing ? Dict(:k => !isempty(extra_C_dims) ? prod(extra_C_dims[sym] for sym in C_only_idcs) : 0) : nothing)
+    # Map base index symbols to the list of logical modes that map to this base index.
+    base_symbols_to_logical_modes = Dict{Symbol, Vector{Int}}()
+    for (logical_mode, sym) in enumerate(output_logical_mode_map)
+        logical_modes = get!(base_symbols_to_logical_modes, sym, Int[])
+        push!(logical_modes, logical_mode)
+    end
 
-    return C_base
+    # Convert the map into a vector of tuples in the form needed for the constructor.
+    output_base_idcs = [(sym, base_symbols_to_logical_modes[sym]) for sym in sort(collect(keys(base_symbols_to_logical_modes)))]
+    output_idcs_vec = [sym for (sym, _) in output_base_idcs]
+    @bp
+
+    # ========== STEP 6 ==========
+    # Call einsum on the base tensors to compute the base tensor of the output.
+
+    output_base = einsum(output_idcs_vec,
+        superTensors[1].base, input_idcs[1],
+        superTensors[2].base, input_idcs[2])
+    @bp
+
+    # ========== STEP 7 ==========
+    # Construct the output SuperTensor.
+
+    # Determine the shape of the output SuperTensor.
+    output_shape = ntuple(i -> 
+        begin
+            output_idx = output_idcs[i]
+            input_tensor_with_idx = findfirst(t -> output_idx in inputs[t][2], 1:length(inputs))
+            axis_in_base = findfirst(==(output_idx), inputs[input_tensor_with_idx][2])
+            size(inputs[input_tensor_with_idx][1], axis_in_base)
+        end, length(output_idcs))
+    @bp
+
+    # Map each logical mode to its corresponding base index symbol.
+    logical_modes_map = Dict{Int, Symbol}()   
+    for (sym, logical_modes) in output_base_idcs
+        for m in logical_modes
+            logical_modes_map[m] = sym
+        end
+    end
+    @bp
+
+    # Construct a map in the right format for the underlying struct.
+    symbol_to_number = Dict{Symbol, Int}()
+    for (i, (sym, _)) in enumerate(output_base_idcs)
+        symbol_to_number[sym] = i
+    end
+    output_map = ntuple(n -> symbol_to_number[logical_modes_map[n]], length(output_shape))
+    @bp
+
+    return SuperTensor(output_shape, output_base, output_map)
 end
